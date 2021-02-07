6dc2bb5046d24a4a0735bee3ce06010ceffb89a9
Kiliosvita <williamxu1998@gmail.com>
2021-02-06 20:57:06 -0500

    adding getUsers route

--- index.js
+++ index.js
@@ -31,11 +31,15 @@ const rooms = {};
 // Routes
 //server.use(bodyParser);
 
+//-------------------Create Room-----------------------------
 server.post('/createroom', (request, response) => {
   var randomwords = require('random-words');
   var randomnumber = require('random-number');
   var code = randomwords({ exactly: 2, join: '-' }).concat('-', randomnumber({ min: 1, max: 9, integer: true }));
 
+  //Subscribing to topic
+  app.subscribeToTopic(code)
+  
   rooms[code] = {
     users: [],
     videoQueue: [],
@@ -50,20 +54,43 @@ server.post('/createroom', (request, response) => {
   });
 });
 
+//-------------------Join Room-----------------------------
 server.post('/joinroom', bodyParser.json(), (request, response) => {
   console.log(request.body);
 
   //Cannot locate room
   if (!(request.body['roomcode'] in rooms)) {
     response.status(400).json({ error: 'error joining room' });
+    console.log("Error joining room - No Roomcode Exist");
     return;
   }
 
   //Adding user to room
   rooms[request.body['roomcode']]['users'].push(request.body['username']);
   response.status(200).json({ message: 'room joined' });
-
+  
+  //Publishing join message
+  app.publishMessage(request.body['roomcode'], request.body['username'].concat(" joined the room."))
+  console.log("Publishing join message")
+  
   console.log(rooms);
+});
+
+//-------------------Get Users-----------------------------
+server.get('/getUsers', bodyParser.json(), (request, response) => {
+  console.log(request.body);
+  
+  //Cannot locate room
+  if (!(request.body['roomcode'] in rooms)) {
+    response.status(400).json({ error: 'Error - no room exist' });
+    console.log("Error - No Roomcode Exist");
+    return;
+  }
+  
+  //Sending list of users
+  response.status(200).json({
+    message: request.body['roomcode']['users']
+  });
 });
 
 server.listen(port, () => console.log(`Listening on ${port}`));

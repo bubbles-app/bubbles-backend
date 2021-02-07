319e2bc4408f5522c1237bba0822a1aa3863ccac
Thomas H. Li <34053587+thomas-hai-li@users.noreply.github.com>
2021-02-06 21:10:45 -0500

    fix /getusers

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ server.post('/createroom', (request, response) => {
   var code = randomwords({ exactly: 2, join: '-' }).concat('-', randomnumber({ min: 1, max: 9, integer: true }));
 
   //Subscribing to topic
-  app.subscribeToTopic(code)
-  
+  app.subscribeToTopic(code);
+
   rooms[code] = {
     users: [],
     videoQueue: [],
@@ -61,35 +61,37 @@ server.post('/joinroom', bodyParser.json(), (request, response) => {
   //Cannot locate room
   if (!(request.body['roomcode'] in rooms)) {
     response.status(400).json({ error: 'error joining room' });
-    console.log("Error joining room - No Roomcode Exist");
+    console.log('Error joining room - No Roomcode Exist');
     return;
   }
 
   //Adding user to room
   rooms[request.body['roomcode']]['users'].push(request.body['username']);
   response.status(200).json({ message: 'room joined' });
-  
+
   //Publishing join message
-  app.publishMessage(request.body['roomcode'], request.body['username'].concat(" joined the room."))
-  console.log("Publishing join message")
-  
+  app.publishMessage(request.body['roomcode'], request.body['username'].concat(' joined the room.'));
+  console.log('Publishing join message');
+
   console.log(rooms);
 });
 
 //-------------------Get Users-----------------------------
-server.get('/getUsers', bodyParser.json(), (request, response) => {
+server.post('/getUsers', bodyParser.json(), (request, response) => {
   console.log(request.body);
-  
+
   //Cannot locate room
   if (!(request.body['roomcode'] in rooms)) {
     response.status(400).json({ error: 'Error - no room exist' });
-    console.log("Error - No Roomcode Exist");
+    console.log('Error - No Roomcode Exist');
     return;
   }
-  
+
+  const roomcode = request.body['roomcode'];
+
   //Sending list of users
   response.status(200).json({
-    message: request.body['roomcode']['users']
+    users: rooms[roomcode]['users']
   });
 });
 
